5fd33e634d9dce5272c5209392371e3beec2e199
fire-monke <148074242+fire-monke@users.noreply.github.com>
2023-11-13 17:50:32 +0100

    Add files via upload

---
+++ insert_ecuries.sql
@@ -0,0 +1,13 @@
+Use bdTheNorthRace;
+
+INSERT INTO Pilote VALUES
+("Red Bull Racing", NULL, NULL),
+("Mercedes", NULL, NULL),
+("Ferrari", NULL, NULL),
+("McLaren", NULL, NULL),
+("Aston Martin", NULL, NULL),
+("Alpine", NULL, NULL),
+("Williams", NULL, NULL),
+("AlphaTauri", NULL, NULL),
+("Alpha Romeo", NULL, NULL),
+("Hass F1 Team", NULL, NULL);
