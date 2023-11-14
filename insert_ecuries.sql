44b1899d8a21150ebe3a60b5c2e8567de2b6a242
fire-monke <148074242+fire-monke@users.noreply.github.com>
2023-11-14 17:47:07 +0100

    Update insert_ecuries.sql
    ajout des insertions

--- insert_ecuries.sql
+++ insert_ecuries.sql
@@ -1,13 +1,84 @@
-Use bdTheNorthRace;
+USE bdTheNorthRace;
 
-INSERT INTO Pilote VALUES
-("Red Bull Racing", NULL, NULL),
-("Mercedes", NULL, NULL),
-("Ferrari", NULL, NULL),
-("McLaren", NULL, NULL),
-("Aston Martin", NULL, NULL),
-("Alpine", NULL, NULL),
-("Williams", NULL, NULL),
-("AlphaTauri", NULL, NULL),
-("Alpha Romeo", NULL, NULL),
-("Hass F1 Team", NULL, NULL);
+-- 2018
+INSERT INTO Ecurie(nom) VALUES
+('Mercedes'),
+('Ferrari'),
+('Renault'),
+('Red Bull Racing'),
+('Haas'),
+('McLaren Renault'),
+('Force India Mercedes'),
+('Sauber'),
+('Scuderia Toro Rosso'),
+('Williams'),
+('Force India Sahara');
+
+-- 2019
+INSERT INTO Ecurie(nom) VALUES
+-- ('MERCEDES'),
+-- ('FERRARI'),
+('Red Bull Racing'),
+-- ('MCLAREN RENAULT'),
+-- ('RENAULT'),
+-- ('SCUDERIA TORO ROSSO HONDA'),
+('Racing Point BWT'),
+('Alfa Romeo Racing')
+-- ('HAAS FERRARI'),
+-- ('WILLIAMS MERCEDES'),
+;
+
+-- 2020
+INSERT INTO Ecurie(nom) VALUES
+-- ('MERCEDES'),
+-- ('RED BULL RACING HONDA'),
+-- ('MCLAREN RENAULT'),
+-- ('RACING POINT BWT MERCEDES'),
+-- ('RENAULT'),
+-- ('FERRARI'),
+('Alphatauri')
+-- ('ALFA ROMEO RACING FERRARI'),
+-- ('HAAS FERRARI'),
+-- ('WILLIAMS MERCEDES'),
+;
+
+-- 2021
+INSERT INTO Ecurie(nom) VALUES
+-- ('MERCEDES'),
+-- ('RED BULL RACING HONDA'),
+-- ('FERRARI'),
+('McLaren Mercedes'),
+('Alpine'),
+-- ('ALPHATAURI HONDA'),
+('Aston Martin')
+-- ('WILLIAMS MERCEDES'),
+-- ('ALFA ROMEO RACING FERRARI'),
+-- ('HAAS FERRARI')
+;
+
+-- 2022
+INSERT INTO Ecurie(nom) VALUES
+('Red Bull Racing'),
+-- ('FERRARI'),
+-- ('MERCEDES'),
+-- ('Alpine Renault'),
+-- ('McLaren Mercedes'),
+('Alfa Romeo Ferrari'),
+('Aston Martin Aramco'),
+-- ('HAAS FERRARI')
+('Alphatauri')
+-- ('WILLIAMS MERCEDES'),
+;
+
+-- 2023
+INSERT INTO Ecurie(nom) VALUES
+('Red Bull Racing'),
+-- ('Mercedes'), (Commenté car déjà présent dans 2018)
+-- ('Ferrari'), (Commenté car déjà présent dans 2018)
+('McLaren'),
+('Aston Martin'),
+('Alpine'),
+('Williams'),
+('Alphatauri'),
+('Alpha Romeo'),
+('Hass');
