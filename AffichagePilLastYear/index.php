7c39929ae094413029306971a11dfc5c6b7d7839
Samih63 <skraria.sio@gmail.com>
2023-12-19 15:22:51 +0100

    ui

---
+++ AffichagePilLastYear/index.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    
+</head>
+<body>
+   <main>
+   <div class="top3">
+        <div class="top1">
+            <div class="nested-div"></div>
+            <div class="nested-div"></div>
+            <div class="nested-div"></div>
+        </div>
+        
+        <div class="top2">
+            <div class="nested-div"></div>
+            <div class="nested-div"></div>
+            <div class="nested-div"></div>
+        </div>
+
+        <div class="top3">
+            <div class="nested-div"></div>
+            <div class="nested-div"></div>
+            <div class="nested-div"></div>
+        </div>
+    </div>
+    <div class="tous">
+
+            <div class="pilote" id="pilote1">
+                <div class="info">
+                    <p>Position: 1</p>
+                    <p>Points: 100</p>
+                </div>
+                <div class="nom-prenom">
+                    <h4>Nom du pilote 1</h4>
+                    <h4>Prénom du pilote 1</h4>
+                    <img src="pays_image1.png" alt="Drapeau du pays du pilote 1" width="50px" height="30px">
+                </div>
+                <div class="ecurie">
+                    <p>Nom de l'écurie 1</p>
+                    <img src="photo_pilote1.png" alt="Photo du pilote 1" width="100px" height="100px">
+                    <p>Numéro: 1</p>
+                </div>
+            </div>
+            <div class="pilote" id="pilote1">
+                <div class="info">
+                    <p>Position: 1</p>
+                    <p>Points: 100</p>
+                </div>
+                <div class="nom-prenom">
+                    <h4>Nom du pilote 1</h4>
+                    <h4>Prénom du pilote 1</h4>
+                    <img src="pays_image1.png" alt="Drapeau du pays du pilote 1" width="50px" height="30px">
+                </div>
+                <div class="ecurie">
+                    <p>Nom de l'écurie 1</p>
+                    <img src="photo_pilote1.png" alt="Photo du pilote 1" width="100px" height="100px">
+                    <p>Numéro: 1</p>
+                </div>
+            </div>
+            <div class="pilote" id="pilote1">
+                <div class="info">
+                    <p>Position: 1</p>
+                    <p>Points: 100</p>
+                </div>
+                <div class="nom-prenom">
+                    <h4>Nom du pilote 1</h4>
+                    <h4>Prénom du pilote 1</h4>
+                    <img src="pays_image1.png" alt="Drapeau du pays du pilote 1" width="50px" height="30px">
+                </div>
+                <div class="ecurie">
+                    <p>Nom de l'écurie 1</p>
+                    <img src="photo_pilote1.png" alt="Photo du pilote 1" width="100px" height="100px">
+                    <p>Numéro: 1</p>
+                </div>
+            </div>
+            <div class="pilote" id="pilote1">
+                <div class="info">
+                    <p>Position: 1</p>
+                    <p>Points: 100</p>
+                </div>
+                <div class="nom-prenom">
+                    <h4>Nom du pilote 1</h4>
+                    <h4>Prénom du pilote 1</h4>
+                    <img src="pays_image1.png" alt="Drapeau du pays du pilote 1" width="50px" height="30px">
+                </div>
+                <div class="ecurie">
+                    <p>Nom de l'écurie 1</p>
+                    <img src="photo_pilote1.png" alt="Photo du pilote 1" width="100px" height="100px">
+                    <p>Numéro: 1</p>
+                </div>
+            </div>
+            <div class="pilote" id="pilote1">
+                <div class="info">
+                    <p>Position: 1</p>
+                    <p>Points: 100</p>
+                </div>
+                <div class="nom-prenom">
+                    <h4>Nom du pilote 1</h4>
+                    <h4>Prénom du pilote 1</h4>
+                    <img src="pays_image1.png" alt="Drapeau du pays du pilote 1" width="50px" height="30px">
+                </div>
+                <div class="ecurie">
+                    <p>Nom de l'écurie 1</p>
+                    <img src="photo_pilote1.png" alt="Photo du pilote 1" width="100px" height="100px">
+                    <p>Numéro: 1</p>
+                </div>
+            </div>
+            <div class="pilote" id="pilote1">
+                <div class="info">
+                    <p>Position: 1</p>
+                    <p>Points: 100</p>
+                </div>
+                <div class="nom-prenom">
+                    <h4>Nom du pilote 1</h4>
+                    <h4>Prénom du pilote 1</h4>
+                    <img src="pays_image1.png" alt="Drapeau du pays du pilote 1" width="50px" height="30px">
+                </div>
+                <div class="ecurie">
+                    <p>Nom de l'écurie 1</p>
+                    <img src="photo_pilote1.png" alt="Photo du pilote 1" width="100px" height="100px">
+                    <p>Numéro: 1</p>
+                </div>
+            </div>
+            <div class="pilote" id="pilote1">
+                <div class="info">
+                    <p>Position: 1</p>
+                    <p>Points: 100</p>
+                </div>
+                <div class="nom-prenom">
+                    <h4>Nom du pilote 1</h4>
+                    <h4>Prénom du pilote 1</h4>
+                    <img src="pays_image1.png" alt="Drapeau du pays du pilote 1" width="50px" height="30px">
+                </div>
+                <div class="ecurie">
+                    <p>Nom de l'écurie 1</p>
+                    <img src="photo_pilote1.png" alt="Photo du pilote 1" width="100px" height="100px">
+                    <p>Numéro: 1</p>
+                </div>
+            </div>
+    </div>
+</body>
+</html>
