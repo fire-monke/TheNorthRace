09c51d6378ad784c4509aeae8a379e6d3569a8e6
Anto <Anto@ANTORDI>
2023-12-28 12:55:07 +0100

    t

--- controller/back/login_controller.php
+++ controller/back/login_controller.php
@@ -7,30 +7,24 @@ if($_SERVER["SCRIPT_FILENAME"] == __FILE__ || !isset($racine) || empty($racine) 
     $racine = "../../";
 }
 
-
-session_start();
-
 if (isset($_POST['submit'])) {
     $identifiant = $_POST['identifiant'];
     $password = $_POST['password'];
 
-    echo $racine;
+    require_once("../../getRacine.php");
     require_once("$racine/model/back/request.php");
-    
     $getAdmin = new GetModeles();
     $admin = $getAdmin->GetAdmin($identifiant);
 
     if ($admin && $password === $admin['password']) {
         $_SESSION['admin'] = $admin;
-        // header('Location: ./view/back/path/app.php');
-        
-        include_once("$racine/view/back/path/app.php");
+        header('Location: ../../view/back/path/app.php');
         exit();
     } else {
         $error = 'Identifiants incorrects. Veuillez réessayer.';
     }
 }
-// require_once("$racine/view/back/login.php");
 
+// include_once '../../view/back/login.php';
 ?>
 
