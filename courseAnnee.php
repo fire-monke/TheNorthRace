721d553eb45173f9dcdeecd84ae1fa394995ef71
Samih63 <146714972+Samih63@users.noreply.github.com>
2024-02-12 09:56:06 +0100

    Create courseAnnee.php

---
+++ courseAnnee.php
@@ -0,0 +1,48 @@
+<?php
+require_once(RACINE . '/model/connexion_PDO.php');
+
+class CourseManager {
+    private $cnx;
+
+    public function __construct() {
+        $this->cnx = connectDB();
+    }
+     public function getCourseDetailsByPilotAndYear($idPil, $year) {
+        $req = $this->pdo->prepare("SELECT * FROM CoursesAnnee WHERE idPil = :idPil AND annee = :year");
+        $req->bindParam(':idPil', $idPil, PDO::PARAM_INT);
+        $req->bindParam(':year', $year, PDO::PARAM_INT);
+        $req->execute();
+        return $req->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function getCourseDetailsByTeamAndYear($idEcu, $year) {
+        $req = $this->pdo->prepare("SELECT * FROM CoursesAnnee WHERE idEcu = :idEcu AND annee = :year");
+        $req->bindParam(':idEcu', $idEcu, PDO::PARAM_INT);
+        $req->bindParam(':year', $year, PDO::PARAM_INT);
+        $req->execute();
+        return $req->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function getCourseDetailsByPilotTeamAndYear($idPil, $idEcu, $year) {
+        $req = $this->pdo->prepare("SELECT * FROM CoursesAnnee WHERE idPil = :idPil AND idEcu = :idEcu AND annee = :year");
+        $req->bindParam(':idPil', $idPil, PDO::PARAM_INT);
+        $req->bindParam(':idEcu', $idEcu, PDO::PARAM_INT);
+        $req->bindParam(':year', $year, PDO::PARAM_INT);
+        $req->execute();
+        return $req->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function getTop3CoursesByYear($year) {
+        $req = $this->pdo->prepare("SELECT * FROM CoursesAnnee WHERE annee = :year ORDER BY nbPointPil DESC LIMIT 3");
+        $req->bindParam(':year', $year, PDO::PARAM_INT);
+        $req->execute();
+        return $req->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function getAllCoursesByYear($year) {
+        $req = $this->pdo->prepare("SELECT * FROM CoursesAnnee WHERE annee = :year");
+        $req->bindParam(':year', $year, PDO::PARAM_INT);
+        $req->execute();
+        return $req->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
