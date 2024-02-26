9889a3253d868b3f86cd28551038b92162e5abd6
Anto <Anto@ANTORDI>
2024-02-26 14:46:11 +0100

    lang en -> fr

--- view/front/path/partials/header.php
+++ view/front/path/partials/header.php
@@ -3,7 +3,7 @@
 include_once(RACINE . '/controller/front//header_controller.php');
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
